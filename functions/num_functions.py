03467ead3be11ed21fb1a705e3835c50c78c6fd7
Abdulkadir Temizoglu <tmzglbdlkdr@gmail.com>
2025-12-07 18:53:51 +0300

    Fonksiyon Dosyası Oluşturdum

---
+++ functions/num_functions.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+from scipy import stats
+from scipy.stats import chi2_contingency
+from itertools import combinations
+import warnings
+
+warnings.filterwarnings('ignore')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    print("This file contains numerical analysis functions.")
